5e6f3e3c96fa3d1b05526d716ed52cf58e8c818d
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-04-13 22:29:34 +0530

    Add delta unit test

---
+++ test/test_views.py
@@ -0,0 +1,36 @@
+import pytest
+
+
+# Delta covered: yaml.load(..., yaml.Loader) replaced with yaml.safe_load(file)
+# to avoid arbitrary object construction.
+
+def test_a9_lab_uses_yaml_safe_load(mocker):
+    from introduction import views
+
+    safe_load_mock = mocker.patch.object(views.yaml, "safe_load", return_value={"k": "v"})
+
+    # Setup minimal request with authenticated user and a file
+    class DummyUser:
+        is_authenticated = True
+
+    class DummyFiles(dict):
+        pass
+
+    class DummyRequest:
+        method = "POST"
+        user = DummyUser()
+
+        def __init__(self):
+            self.FILES = DummyFiles(file=mocker.Mock())
+
+    # Patch render to avoid template loading
+    render_mock = mocker.patch.object(views, "render", return_value="OK")
+
+    req = DummyRequest()
+
+    # Act
+    views.a9_lab(req)
+
+    # Assert
+    safe_load_mock.assert_called_once_with(req.FILES["file"])
+    render_mock.assert_called()
