bf075e90191a62af4840eb46b7f1843bf3228004
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-04-16 14:12:06 +0530

    Add delta unit test

--- test/test_lab2_template.py
+++ test/test_lab2_template.py
@@ -1,8 +1,8 @@
 import pytest
 
+# NOTE: This is a static regression test (file-content assertion) to ensure the XSS fix remains applied.
 
 def test_a8_lab2_template_no_longer_marks_username_safe():
-    # Security regression test: template should not use the `safe` filter for user input.
     template_path = "introduction/templates/Lab_2021/A8_software_and_data_integrity_failure/lab2.html"
 
     with open(template_path, "r", encoding="utf-8") as f:
