184d762ed990ff7b27cfa888c1d1f4198852f3e9
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-04-16 14:12:10 +0530

    Add delta unit test

--- test/test_insec_des_base_template.py
+++ test/test_insec_des_base_template.py
@@ -1,8 +1,8 @@
 import pytest
 
+# NOTE: This is a static regression test (file-content assertion) to ensure theme validation remains applied.
 
 def test_insec_des_base_template_sanitizes_theme_value():
-    # Static regression test: only 'dark'/'light' should be used when setting theme from localStorage.
     template_path = "dockerized_labs/insec_des_lab/templates/base.html"
 
     with open(template_path, "r", encoding="utf-8") as f:
