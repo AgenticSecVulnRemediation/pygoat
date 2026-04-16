f38093cdbf61f66f40778e7581794a10a612e3ac
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-04-16 14:12:14 +0530

    Add delta unit test

--- test/test_views_xxe_import.py
+++ test/test_views_xxe_import.py
@@ -1,11 +1,8 @@
-import types
-
 import pytest
 
 # Assumption: repository root is on PYTHONPATH in test runner.
 from introduction import views
 
 
-def test_xxe_parse_uses_defusedxml_make_parser_import():
-    # Regression test for XXE hardening change: make_parser import should come from defusedxml.sax
+def test_views_uses_defusedxml_sax_make_parser():
     assert views.make_parser.__module__.startswith("defusedxml")
