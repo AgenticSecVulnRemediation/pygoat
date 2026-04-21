4888eff74308576e9690817ad7216350e829eab8
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-04-21 16:34:13 +0530

    Add delta unit test

--- test/ssrf.test.js
+++ test/ssrf.test.js
@@ -2,14 +2,14 @@
  * @jest-environment jsdom
  */
 
-describe('ssrf.js checkcode input sanitization (html only)', () => {
+describe('ssrf.js checkcode sanitization (html only)', () => {
   beforeEach(() => {
     document.body.innerHTML = `
       <textarea id="python"></textarea>
       <textarea id="html"></textarea>
     `;
-    jest.resetModules();
 
+    jest.resetModules();
     global.DOMPurify = { sanitize: jest.fn((x) => `SANITIZED:${x}`) };
     global.fetch = jest.fn(() => Promise.resolve({ text: () => Promise.resolve('{"passed":0,"message":"ok"}') }));
 
