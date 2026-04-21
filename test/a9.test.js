1ab30dbd8cd6f7e1ad5058ab7b01b54386069f8f
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-04-21 16:33:23 +0530

    Add delta unit test

--- test/a9.test.js
+++ test/a9.test.js
@@ -2,17 +2,18 @@
  * @jest-environment jsdom
  */
 
-describe('a9.js sanitizeInput usage', () => {
+describe('a9.js sanitizeInput', () => {
   beforeEach(() => {
     document.body.innerHTML = `
       <input id="a9_log" value="<script>alert(1)</script>" />
       <input id="a9_api" value="<b>hi</b>" />
       <ul id="a9_d3"></ul>
     `;
+
     jest.resetModules();
   });
 
-  test('sanitizes log_code and api_code before sending', async () => {
+  test('sanitizes fields before putting them into FormData', async () => {
     const fetchSpy = jest.fn(() =>
       Promise.resolve({
         text: () => Promise.resolve(JSON.stringify({ logs: [] })),
@@ -20,7 +21,6 @@ describe('a9.js sanitizeInput usage', () => {
     );
     global.fetch = fetchSpy;
 
-    // Minimal FormData shim to inspect appended fields in Node.
     const fields = {};
     global.FormData = class {
       append(k, v) {
