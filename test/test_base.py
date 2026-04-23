748923ebb6d5950a8f32b0efbb8ebda136994186
rohanSolanki97 <rohan.solanki@hcltech.com>
2026-04-23 13:24:45 +0530

    Add delta unit test

---
+++ test/test_base.py
@@ -0,0 +1,9 @@
+import pytest
+
+# Note: This project appears to use a Django-style structure but also contains a Flask/Jinja template.
+# Since base.html is a template (not Python/JS/TS/Java), unit test generation is not applicable.
+
+
+def test_placeholder():
+    # This test file is intentionally minimal; the corresponding change was in an HTML template.
+    assert True
